35e7d9a5174ec780f4ae431ac238008f4ac2d2ba
deepalishivanagi <deepalishivanagi@gmail.com>
2020-07-19 13:44:57 +0530

    fixed profile page retrival

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -1,9 +1,9 @@
 angular.module('ProfileCtrl', []).controller('ProfileController', function($scope,$http) {
     $http.get('/queryProfile').then(function(item) {
 		console.log(item);
-	//	$scope.data= item.data;
+		$scope.data= item.data;
  
-	// $scope.tagline = 'Hackers are the smart workers';	
+	 $scope.tagline = 'Hackers are the smart workers';	
 
 });
 });
